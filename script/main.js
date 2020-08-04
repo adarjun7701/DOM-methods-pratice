ce272f33979a79d267821e48bf53db0f6ed55211
adarjun7701 <63601256+adarjun7701@users.noreply.github.com>
2020-08-04 14:04:50 -0400

    Add commit

---
+++ script/main.js
@@ -0,0 +1,32 @@
+const newBody = document.querySelector('body')
+
+
+
+const BtnTag  = document.createElement('button')
+newBody.append(BtnTag)
+
+
+
+const newMain = document.createElement('main')
+newBody.append(newMain)
+
+
+const newImg = document.createElement('image')
+newImg.src = ""
+newMain.append(newImg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
