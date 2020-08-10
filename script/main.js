e5e4d53edcfbc17362f5e72011b685228698e809
adarjun7701 <63601256+adarjun7701@users.noreply.github.com>
2020-08-10 07:50:45 -0400

    My final committment

--- script/main.js
+++ script/main.js
@@ -1,22 +1,48 @@
-const newBody = document.querySelector('body')
+
+const init = () => {
+    main = document.createElement("main");
+    button = createButton();
+    link = createLink();
+    image = createImage();
+    main.append(image);
+    main.append(link);
+    document.body.append(button);
+    document.body.append(main);
+
+    registerEvents();
+}
+
+const createButton = () => {
+    const btn = document.createElement("button");
+    btn.append("Click Me");
+    return btn;
+}
 
 
 
-const BtnTag  = document.createElement('button')
-newBody.append(BtnTag)
 
+const createImage = () => {
+    const img = document.createElement("img");
+    img.src = "./script/Myimg.jpg";
+    img.classList = ["image"];
+    return img;
+}
 
+const createLink = () => {
+    const link = document.createElement("a");
+    link.classList = ["link"];
+    link.href = "https://www.google.com/";
+    link.append("Link to Google");
+    return link;
+}
 
-const newMain = document.createElement('main')
-newBody.append(newMain)
+function registerEvents() {
+    button.addEventListener('click', function() {
+        document.body.removeChild(main);
+    })
+}
 
-
-const newImg = document.createElement('image')
-newImg.src = ""
-newMain.append(newImg)
-
-
-
+document.addEventListener('DOMContentLoaded', init);
 
 
 
